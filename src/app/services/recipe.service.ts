414a958be5b6989812933a16ad1de0e3d9556c35
louisehedman <louise.hedman@chasacademy.se>
2022-04-04 09:52:40 +0200

    add routes for recipe lists

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -30,7 +30,24 @@ export class RecipeService {
     return this.http.get<any>(this.apiURL + '/' + id + '/information?' + this.api_auth)
   }
 
-  
+  getAllRecipeLists(): Observable<any> {
+    return this.http.get<any>('https://randomrecipeappu06.herokuapp.com/api/auth/recipe-lists')
+  }
 
+  getOneRecipeList(id: number): Observable<any> {
+    return this.http.get<any>('https://randomrecipeappu06.herokuapp.com/api/auth/recipe-lists/' + id)
+  }
+
+  createRecipeList(name: string): Observable<any> {
+    return this.http.post<any>('https://randomrecipeappu06.herokuapp.com/api/auth/recipe-lists', { name: name})
+  }
+
+  updateRecipeList(id: number, name: string, recipes_id: string): Observable<any> {
+    return this.http.put<any>('https://randomrecipeappu06.herokuapp.com/api/auth/recipe-lists/' + id, {name: name, recipes_id: recipes_id})
+  }
+
+  deleteRecipeList(id: number): Observable<any> {
+    return this.http.delete<any>('https://randomrecipeappu06.herokuapp.com/api/auth/recipe-lists/' + id)
+  }
   
 }
