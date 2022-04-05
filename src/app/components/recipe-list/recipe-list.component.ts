bb3c499e77a827cc74f497af647b513e78450455
louisehedman <louise.hedman@chasacademy.se>
2022-04-05 18:11:41 +0200

    add html for recipe list and initial structure recipe-list component

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { RecipeListService } from 'src/app/services/recipe-list.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { RecipeList } from '../../models/recipeList';
+import { Recipe } from '../../models/recipe';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipeListId!: number;
+recipesId: any = [];
+recipeList!: RecipeList;
+recipe: Recipe[] = [];
 
-  constructor() { }
+constructor(public recipeListService: RecipeListService, public recipeService: RecipeService, public route: ActivatedRoute, public router: Router) { }
 
   ngOnInit(): void {
+      this.recipeListId = this.route.snapshot.params['recipeListId'];
+           
+      this.recipeListService.getOneRecipeList(this.recipeListId).subscribe((data: RecipeList)=>{
+        this.recipeList = data;
+      });
+  
+
   }
 
 }
