a497a5fa4ed5f3d9dac64e3ffed14b4a3389da55
louisehedman <louise.hedman@chasacademy.se>
2022-04-11 16:38:20 +0200

    change api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // Key for Spoonacular API
-  api_key: 'a2fb12d2cc514e6298ef23ceeb46b1d0',
+  api_key: '322dfdd7a61b4981822dbd8ebc5fc566'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
     // Key for Spoonacular API
-  api_key: 'a2fb12d2cc514e6298ef23ceeb46b1d0',
+  api_key: '322dfdd7a61b4981822dbd8ebc5fc566'
 };
 
 /*
